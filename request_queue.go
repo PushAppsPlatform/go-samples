8bc248e4eaf16014c1bc6dc70d87aedccf971e3c
Orr Chen <orrchen@users.noreply.github.com>
2016-08-31 13:30:07 +0200

    Update request_queue.go
    working example

--- request_queue.go
+++ request_queue.go
@@ -1,27 +1,47 @@
+package main
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	"errors"
+	"github.com/bugsnag/bugsnag-go"
+	"encoding/json"
+)
+
+var semaphore chan bool
+var maxRequestLength int
+
 func main() {
 	semaphore = make(chan bool, 10)
 	for i := 0; i < cap(semaphore); i++ {
 		semaphore <- true
 	}
+	bugsnag.Configure(bugsnag.Configuration{
+		APIKey: "API KEY",
+		ReleaseStage: "test"})
 	r := mux.NewRouter()
-	r.Handle("/version", RecoverWrap(requestQueueHandler(http.HandlerFunc(Version)))).Methods("GET")
+	r.Handle("/version", recoverWrap(requestQueueHandler(http.HandlerFunc(Version)))).Methods("GET")
+	http.Handle("/", r)
+	err := http.ListenAndServe(":3001", nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func requestQueueHandler(fn http.Handler) http.HandlerFunc {
 	return func(rw http.ResponseWriter, req *http.Request) {
 		<-semaphore
-		if (cap(semaphore)- len(semaphore)) > maxRequestLength {
-			maxRequestLength = cap(semaphore)- len(semaphore)
+		if (cap(semaphore) - len(semaphore)) > maxRequestLength {
+			maxRequestLength = cap(semaphore) - len(semaphore)
 		}
-		fn.ServeHTTP(rw,req)
+		fn.ServeHTTP(rw, req)
 	}
 }
 
-func RecoverWrap(h http.Handler) http.Handler {
+func recoverWrap(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		var err error
 		defer func() {
-			semaphore<- true
+			semaphore <- true
 			r := recover()
 			if r != nil {
 				switch t := r.(type) {
@@ -32,10 +52,23 @@ func RecoverWrap(h http.Handler) http.Handler {
 				default:
 					err = errors.New("Unknown error")
 				}
-				bugsnag.Notify(err,req)
+				bugsnag.Notify(err, req)
 				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 			}
 		}()
 		h.ServeHTTP(w, req)
 	})
 }
+
+func Version(writer http.ResponseWriter, req *http.Request) {
+	res := VersionResponse{Version: "1"}
+	js, _ := json.Marshal(res)
+	writer.Header().Set("Content-Type", "application/json")
+	writer.Write(js)
+}
+
+type VersionResponse struct {
+	Version string `json:"version"`
+}
+
+
